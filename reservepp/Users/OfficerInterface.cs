a9dc9840874344cc9fc9eedc54f8f4aa4c78e740
Max Shapovalov <shapovalov3334@gmail.com>
2025-03-04 09:37:56 +0200

    Модернізовано програму

--- reservepp/Users/OfficerInterface.cs
+++ reservepp/Users/OfficerInterface.cs
@@ -3,7 +3,5 @@ public interface OfficerInterface
     void makeOffer(int amount);
     void returnSoldier(int docID);
     void returnSoldiers(List<int> docsID);
-
-    //todo
-
+    void displayInfo();
 }
--- reservepp/Users/User.cs
+++ reservepp/Users/User.cs
@@ -3,41 +3,49 @@ abstract class User
 	int age, docID;
     string firstName, lastName, medicalExaminationResult, city;
     bool hasDeferment;
-   
-	User(std::string firstName, std::string lastName, int age, int docID, std::string medicalExaminationResult, bool HasDeferment, std::string city) { };
+    
+	User(std::string firstName, std::string lastName, int age, int docID, std::string medicalExaminationResult, bool HasDeferment, std::string city) {
+        this->firstName = firstName;
+        this->lastName = lastName;
+        this->age = age;
+        this->docID = docID;
+        this->medicalExaminationResult = medicalExaminationResult;
+        this->hasDeferment = hasDeferment;
+        this->city = city;
+    };
 
     /*
     SETTERS
     */
-    void setAge(int age)
+    public void setAge(int age)
     {
         this->age = age;
     }
-    void setFirstName(string firstName)
+    public void setFirstName(string firstName)
     {
         this->firstName = firstName;
     }
-    void setLastName(string lastName)
+    public void setLastName(string lastName)
     {
         this->lastName = lastName;
     }
-    void setDocID(int age)
+    public void setDocID(int age)
     {
         this->age = age;
     }
-    void setMedicalExaminationResult(string medicalExaminationResult)
+    public void setMedicalExaminationResult(string medicalExaminationResult)
     {
         this->medicalExaminationResult = medicalExaminationResult;
     }
-    void setHasDeferment(bool HasDeferment)
+    public void setHasDeferment(bool HasDeferment)
     {
         this->hasDeferment = hasDeferment;
     }
-    void setCity(string city)
+    public void setCity(string city)
     {
         this->city = city;
     }
-    void setData(string firstName, string lastName, int age, int docID, string medicalExaminationResult, bool hasDeferment, string city)
+    public void setData(string firstName, string lastName, int age, int docID, string medicalExaminationResult, bool hasDeferment, string city)
     {
         this->firstName = firstName;
         this->lastName = lastName;
@@ -50,40 +58,43 @@ abstract class User
     /*
     GETTERS
     */
-    int getAge()
+    public int getAge()
     {
         return age;
     }
-    string firstName()
+    public string firstName()
     {
         return firstName;
     }
-    string lastName()
+    public string lastName()
     {
         return lastName;
     }
-    string getName()
+    public string getName()
     {
         return firstName + " " + lastName;
     }
-    int getDocID()
+    public int getDocID()
     {
         return docID;
     }
-    string getMedicalExaminationResult()
+    public string getMedicalExaminationResult()
     {
         return medicalExaminationResult;
     }
-    bool getHasDeferment()
+    public bool getHasDeferment()
     {
         return hasDeferment;
     }
-    string getCity()
+    public string getCity()
     {
         return city;
     }
-    void getData()
+    public void getData()
     {
 
     }
+
+    public abstract void displayInfo();
+
 }
