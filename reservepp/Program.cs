24f03130f2e2c3c47bcd7eaa8e2a867d9ad664a2
Max Shapovalov <shapovalov3334@gmail.com>
2025-03-18 12:11:38 +0200

    refact: Українізація

--- reservepp/Program.cs
+++ reservepp/Program.cs
@@ -19,35 +19,36 @@ namespace reservepp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Form1());
+            Application.Run(new RegisterForm());
+
             IRepository<Product> productRepository = new Repository<Product>();
             var productService = new UserService(productRepository);
 
-            // Добавляем продукты
+            // Додавання продукту
             var apple = new Product { Name = "Apple" };
             var banana = new Product { Name = "Banana" };
 
             productService.AddProduct(apple);
             productService.AddProduct(banana);
 
-            // Выводим все продукты
+            // Виводимо всі продукти
             MessageBox.Show("Products after adding:", "Сообщение");
             foreach (var product in productService.GetAllProducts())
             {
                 MessageBox.Show($"ID: {product.Id}, Name: {product.Name}", "Сообщение");
             }
 
-            // Удаляем первый продукт
+            // Видаляємо перший продукт 
             productService.DeleteProduct(apple.Id);
 
-            // Выводим продукты после удаления
+            // Виводимо продукти після видалення
             
             MessageBox.Show("After deletion:", "Сообщение");
             foreach (var product in productService.GetAllProducts())
             {
                 MessageBox.Show($"ID: {product.Id}, Name: {product.Name}", "Сообщение");
             }
-            Application.Run(new OfficerForm());
+            
         }
     }
 }
