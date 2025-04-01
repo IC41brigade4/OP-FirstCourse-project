9c8fc626c5464162d4c09f7829eac6d93f963d90
Anastas18 <evgenkucha2006@gmail.com>
2025-04-01 04:59:11 +0300

    users can see and change  information about themselfes

--- reservepp/ConscriptForm.cs
+++ reservepp/ConscriptForm.cs
@@ -45,7 +45,28 @@ namespace reservepp
         {
             this.Inf_panel.Visible = true;
             this.gotit_btn.Visible = true;
-            // need to add inf to textfields
+
+            User user = userRepository.GetById(DocId);
+
+            if (user == null)
+            {
+                MessageBox.Show("Користувач не знайдений!");
+                return;
+            }
+
+            // Оновлення тексту
+            firstname_text.Text = $"First name: {user.FirstName}";
+            lastname_text.Text = $"Last name: {user.LastName}";
+            age_text.Text = $"Age: {user.Age}";
+            docid_text.Text = $"DocID: {user.DocID}";
+            city_text.Text = $"City: {user.City}";
+
+            // Переконайся, що елементи відображаються
+            firstname_text.Show();
+            lastname_text.Show();
+            age_text.Show();
+            docid_text.Show();
+            city_text.Show();
         }
 
         private void change_inf_btn_Click(object sender, EventArgs e)
@@ -56,9 +77,29 @@ namespace reservepp
 
         private void Change_btn_Click(object sender, EventArgs e)
         {
-            // need to save somewhere changed inf
-            this.Inf_panel_change.Visible = false;
-            this.Change_btn.Visible = false;
+            this.Inf_panel_change.Visible = true;
+            this.Change_btn.Visible = true;
+
+            User user = userRepository.GetById(DocId);
+
+            if (user == null)
+            {
+                MessageBox.Show("Користувач не знайдений!");
+                return;
+            }
+
+            user.FirstName = firstname_textbox.Text;
+            user.LastName = lastname_textbox.Text;
+
+            if (!int.TryParse(age_textbox.Text, out int age_int))
+            {
+                MessageBox.Show("Де ти бачив букви заміст віку або мінусовий вік!!");
+                return;
+            }
+            user.Age = age_int;
+            user.City = city_textbox.Text;
+
+            userRepository.Update(user);
         }
 
         private void gotit_btn_Click(object sender, EventArgs e)
